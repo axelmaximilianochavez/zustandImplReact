b90351229531a4f2cbab87f04dc6e69c1a080ef8
Axel Chavez <axel.chavez6@gmail.com>
2024-10-25 22:33:43 +0900

    quizz javaScript

---
+++ zustandImpl/src/App.tsx
@@ -0,0 +1,26 @@
+import { Container, Stack, Typography } from '@mui/material'
+import './App.css'
+import { Start } from './Start'
+import { useQuestionsStore } from './store/questions'
+import { Game } from './Game'
+
+function App() {
+  const questions = useQuestionsStore(state => state.questions)
+  console.log(questions)
+  return (
+    <main>
+      <Container maxWidth='sm'>
+        <Stack direction='row' gap={1} alignItems='center' justifyContent='center'>
+          <Typography variant='h2' component='h1'>
+            JavaScript Quizz
+          </Typography>
+        </Stack>
+
+        {questions.length == 0 && <Start/>}
+        {questions.length > 0 && <Game/>}
+      </Container>
+    </main>
+  )
+}
+
+export default App
---
+++ zustandImpl/src/Game.tsx
@@ -0,0 +1,31 @@
+import { Card, Typography } from "@mui/material"
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import { useQuestionsStore } from "./store/questions"
+import { type Question as QuestionType } from "./types"
+
+const Question = ({info}: {info: QuestionType}) => {
+    return (
+      <Card variant='outlined' sx={{ bgcolor: '#222', p: 2, textAlign: 'left' }}>
+        <Typography variant='h5'>
+            {info.question}
+        </Typography>
+
+        <SyntaxHighlighter language='javascript' style={gradientDark}>
+            {info.code}
+        </SyntaxHighlighter>
+      </Card>
+    )
+}
+
+export const Game = () => {
+    const questions = useQuestionsStore(state => state.questions)
+    const currentQuestion = useQuestionsStore(state => state.currentQuestion)
+
+    const questionInfo = questions[currentQuestion]
+    return (
+        <>
+          <Question info={questionInfo} />
+        </>
+    )
+}
