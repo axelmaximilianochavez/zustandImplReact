0b034439e9435249018b369fa781a05cce71a4e2
Axel Chavez <axel.chavez6@gmail.com>
2024-10-27 22:58:43 +0900

    add footer with global info

---
+++ src/Footer.tsx
@@ -0,0 +1,22 @@
+import { useQuestionsStore } from './store/questions'
+
+export const Footer = () => {
+  const questions = useQuestionsStore(state => state.questions)
+
+  let correct = 0
+  let incorrect = 0
+  let unanswered = 0
+
+  questions.forEach(question => {
+    const { userSelectedAnswer, correctAnswer } = question
+    if (userSelectedAnswer == null) unanswered++
+    else if (userSelectedAnswer == correctAnswer) correct++
+    else incorrect++
+  })
+
+  return (
+    <footer style={{ marginTop: '16px' }}>
+      <strong>{`correct ${correct} - incorrect ${incorrect} - unanswered ${unanswered}`}</strong>
+    </footer>
+  )
+}
